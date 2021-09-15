a30bcc1fcbb9ff7e8fb76121b8f349310969abda
tejeshpatel <tejeshpatel@live.com>
2021-09-15 02:43:25 -0500

    updating port number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
